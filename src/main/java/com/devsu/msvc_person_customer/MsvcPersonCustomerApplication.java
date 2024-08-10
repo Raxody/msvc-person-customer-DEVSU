9d239c3edb75b255770610692a3e753ead0952ec
Juan Andrés López <juancho_lopez6@hotmail.com>
2024-08-10 12:23:33 -0500

    Feature: Se inicializa el proyecto

---
+++ src/main/java/com/devsu/msvc_person_customer/MsvcPersonCustomerApplication.java
@@ -0,0 +1,13 @@
+package com.devsu.msvc_person_customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcPersonCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcPersonCustomerApplication.class, args);
+	}
+
+}
